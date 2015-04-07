8fce30e7236e160b252c0f80b9ed604491820805
fdesimini <fdesimini@users.noreply.github.com>
2015-04-07 12:01:18 -0400

    We've added different types of data

--- BitmakerDay2/main.m
+++ BitmakerDay2/main.m
@@ -10,7 +10,49 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        // declared integer variable
+        int productNumber = 100;
+        // declared floating point variable
+        float productWeight = 105.67;
+        // recipe strings
+        NSString* recipeName = @"Pasta Sauce";
+        NSString *recipeName2 = @"Burger";
+        // recipe array datastructure
+        // zero indexed[0,1,2]
+        NSArray *recipesNames = @[@"Eggs Benedict", @"Full Breakfast", @"Coffee"];
+        // array without object literal
+        NSArray *recipeObjectLiteral = [NSArray arrayWithObjects:@"Eggs Benedict", @"Full Breakfast", @"Coffee", nil];
+        // an array of quantities
+        NSArray *quantities = @[@10, @35, @60, @98];
+        quantities [3];
+    
+        // create a 'for' loop
+        
+        for (int i=0; i<100; i++) {
+            NSLog(@"%i", i);
+        }
+    // create a 'while' loop
+        
+        int i = 0;
+        while (i<10) {
+            NSLog(@"i = %i", i);
+            i++;
+        }
+        //create an 'if' statement
+        // create two variables to compare
+        int a = 5;
+        int b = 12;
+        
+        if (a > b) {
+            NSLog(@"a is greater than b");
+        }else {
+            NSLog(@"a is not greater than b");
+        }
+        
+        
+        
+        
+        
         NSLog(@"Hello, World!");
     }
     return 0;
