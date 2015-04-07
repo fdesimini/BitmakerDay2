cb182d20f5708f03c084060f1133a8223944cc66
fdesimini <fdesimini@users.noreply.github.com>
2015-04-07 10:30:17 -0400

    Initial Commit

---
+++ BitmakerDay2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BitmakerDay2
+//
+//  Created by Frank Desimini on 2015-04-07.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
